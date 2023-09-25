a029dbe5084dd715aa761ccd7d53c2bf4721dc70
Saveliy <sava2698@gmail.com>
2023-09-25 17:02:29 +0600

    Created login form

--- src/components/screens/auth/auth.component.js
+++ src/components/screens/auth/auth.component.js
@@ -2,19 +2,46 @@ import renderService from '@/core/services/render.service';
 import template from './auth.template.html';
 import styles from './auth.module.scss';
 import { BaseScreen } from '@/core/component/base-screen.component';
-import { Heading } from '@/components/ui/heading/heading.component';
+import { AuthService } from '@/api/auth.service';
+import { Button } from '@/components/ui/button/button.component';
+import { $R } from '@/core/rquery/rquery.lib';
+import { Field } from '@/components/ui/field/field.component';
 
 export class Auth extends BaseScreen {
+	#isTypeLogin = true;
+
 	constructor() {
 		super({ pageTitle: 'Auth' });
+		this.AuthService = new AuthService();
 	}
+
 	render() {
 		this.element = renderService.htmlToElement(
 			template,
-			[new Heading('Авторизация')],
+			[
+				new Button({
+					children: 'Submit'
+				})
+			],
 			styles
 		);
 
+		$R(this.element)
+			.find('#auth-inputs')
+			.append(
+				new Field({
+					placeholder: 'Enter email',
+					name: 'email',
+					type: 'email'
+				}).render()
+			)
+			.append(
+				new Field({
+					placeholder: 'Enter password',
+					name: 'password',
+					type: 'password'
+				}).render()
+			);
 		return this.element;
 	}
 }
