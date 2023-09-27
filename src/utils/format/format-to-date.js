78cedb078d2e44a4fa61af8b753da8361eeb5d6f
Saveliy <sava2698@gmail.com>
2023-09-27 14:05:13 +0600

    Added transactions block

---
+++ src/utils/format/format-to-date.js
@@ -0,0 +1,13 @@
+/**
+ * Formats a date in the "MMM DD , YYYY" format.
+ *
+ * @param {string} dateString - Date string in the format
+ * "YYYY-MM-DDTHH:mm:ss.ssssZ"
+ * @returns {string} The formatted date string in the format "MMM DD,
+ * YYYY"
+ */
+export function formatDate(dateString) {
+	const date = new Date(dateString);
+	const options = { month: 'short', day: 'numeric', year: 'numeric' };
+	return date.toLocaleDateString('en-US', options);
+}
