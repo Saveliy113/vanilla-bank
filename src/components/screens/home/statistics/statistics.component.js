05483b2e17b3e9c84338bc39385ff50b6f533b2b
Saveliy <sava2698@gmail.com>
2023-09-27 18:29:33 +0600

    Added svg graphic

--- src/components/screens/home/statistics/statistics.component.js
+++ src/components/screens/home/statistics/statistics.component.js
@@ -13,6 +13,7 @@ import { $R } from '@/core/rquery/rquery.lib';
 import { StatisticItem } from './statistic-item/statistic-item.component';
 import { formatToCurrency } from '@/utils/format/format-to-currency';
 import { Store } from '@/core/store/store';
+import { CircleChart } from './circle-chart/circle-chart.component';
 
 export class Statistics extends ChildComponent {
 	constructor() {
@@ -51,6 +52,24 @@ export class Statistics extends ChildComponent {
 		this.#removeListeners();
 	}
 
+	renderChart(income, expense) {
+		const total = income + expense;
+		let incomePercent = (income * 100) / total;
+		let expensePercent = 100 - incomePercent;
+
+		if (income && !expense) {
+			incomePercent = 100;
+			expensePercent = 0.1;
+		}
+
+		if (!income && expense) {
+			incomePercent = 0.1;
+			expensePercent = 100;
+		}
+
+		return new CircleChart(incomePercent, expensePercent).render();
+	}
+
 	fetchData() {
 		this.statisticService.main(data => {
 			if (!data) return;
@@ -61,8 +80,8 @@ export class Statistics extends ChildComponent {
 			const statisticsItemsElement = $R(this.element).find('#statistics-items');
 			statisticsItemsElement.text('');
 
-			// const circleChartElement = $R(this.element).find('#circle-chart');
-			// circleChartElement.text('');
+			const circleChartElement = $R(this.element).find('#circle-chart');
+			circleChartElement.text('');
 
 			statisticsItemsElement
 				.append(
@@ -79,6 +98,8 @@ export class Statistics extends ChildComponent {
 						'purple'
 					).render()
 				);
+
+			circleChartElement.append(this.renderChart(data[0].value, data[1].value));
 		});
 	}
 
